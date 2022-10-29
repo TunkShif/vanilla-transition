a5942fd8ea2e40448cb5922028a7af055076af1d
TunkShif <tunkshif@foxmail.com>
2022-10-29 20:23:04 +0800

    init: first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<section align="center">
+  <h1>Vanilla Transition</h1>
+  <p>
+    <a href="https://vanilla-transition.vercel.app/">Demo</a>
+    |
+    <a href="./index.html" alt="Example">Example</a>
+  </p>
+</section>
+
+An experimental project that provides transition components similar to [Headless UI][0] or [Vue][1] but for Vanilla JavaScript!
+
+[0]: https://headlessui.com/react/transition
+[1]: https://vuejs.org/guide/built-ins/transition.html
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import * as Transition from "./transition"
+import "./style.css"
+  ; (() => {
+    const button = document.querySelector<HTMLButtonElement>("#button-0")!
+    const wrapper = document.querySelector<HTMLDivElement>("#transition-wrapper-0")!
+
+    button.addEventListener("click", () => {
+      const state = wrapper.getAttribute("data-transition-state")!
+      wrapper.setAttribute("data-transition-state", state === "show" ? "hide" : "show")
+    })
+
+    Transition.init(wrapper, wrapper)
+  })()
+  ; (() => {
+    const button = document.querySelector<HTMLButtonElement>("#button-1")!
+    const wrapper = document.querySelector<HTMLDivElement>("#transition-wrapper-1")!
+
+    button.addEventListener("click", () => {
+      wrapper.setAttribute("data-transition-state", "hide")
+      setTimeout(() => {
+        wrapper.setAttribute("data-transition-state", "show")
+      }, 500)
+    })
+
+    Transition.init(wrapper, wrapper)
+  })()
